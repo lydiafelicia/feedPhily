43aaf5c67c92044779d38678ffc95c3e3f682ed8
LYDIA FELICIA <lydiafeliciaa@gmail.com>
2015-08-31 22:45:00 -0400

    footer added

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+$("#submitButton").click(function(){
+    alert("Thank you for contacting us! We will get back to you shortly.");
+});
+
+
+
+$("#submitButton2").click(function(){
+    alert("Thank you for your donation! Together we can bring change into Philadelphia!");
+});
+
+
+
+$("#submitButton-white").click(function(){
+    alert("Thank you for contacting us! We will get back to you shortly.");
+});
+
+
+});
+
